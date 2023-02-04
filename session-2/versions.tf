b1898c8f7509b69b783b8e4250d8f097057215fd
azaa1 <azaa.chi11@gmail.com>
2023-02-04 12:07:09 -0600

    versions.tf added

---
+++ session-2/versions.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = "~> 1.3.0" #Terraform Version
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.53.0" # AWS Provider
+    }
+  }
+}
+
+# Terraform = v1.3.7
+# AWS Provider = v4.53.0
+
+# Semantic Versioning 
+# v1.3.7 
+# 1 = Major
+# 3 = Minor 
+# 7 = Patch
+
+# >= 0.13, < 0.14 = every patch version is included
+# 0.13.0 - 0.13.x
+
+# ~> 0.13 =  0.13.0 - 0.13.x
+# ~> = lazy constraints
