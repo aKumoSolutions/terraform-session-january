205de02e8246045e9c48cd3d866db1530cae2143
azaa1 <azaa.chi11@gmail.com>
2023-02-08 20:51:44 -0600

    updated

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -3,9 +3,32 @@ resource "aws_instance" "main" {
     instance_type = var.instance_type
     vpc_security_group_ids = [ aws_security_group.main_sg.id ] 
     tags = {              
-        Name = var.env
+        Name = var.env-instance
     }
 }
+# dev-instance
+# qa-instance
+# stage-instance
+# prod-instance
+
+resource "aws_instance" "main_frontend" {
+    ami = data.aws_ami.amazon_linux_2.id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [ aws_security_group.main_sg.id ] 
+    tags = {              
+        Name = var.env-frontend-instance
+    } 
+}
+
+
+# dev-frontned-instance
+# qa-frontned-instance
+# stage-frontned-instance
+# prod-frontned-instance
+
+
+# dev-instance vs # dev-frontned-instance
+
 
 
 # References:
