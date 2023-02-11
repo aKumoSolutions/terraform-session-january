53308c7fd0e480b2b7d9c3f7fcdfffbadefe4ddb
azaa1 <azaa.chi11@gmail.com>
2023-02-11 13:08:46 -0600

    count.index added

--- session-5/sg.tf
+++ session-5/sg.tf
@@ -5,12 +5,18 @@ resource "aws_security_group" "main_sg" {
 resource "aws_security_group_rule" "ingress" {
   count = 2 
   type              = "ingress"
-  to_port           = element( var.ports, 0 ) # 22
-  from_port         = element( var.ports, 0 ) # 22
+  to_port           = element( var.ports, count.index ) # 22
+  from_port         = element( var.ports, count.index ) # 22
   cidr_blocks = ["0.0.0.0/0"]
   protocol          = "tcp"
   security_group_id = aws_security_group.main_sg.id
 }
+
+# count = 2
+# count.index = 0, 1
+
+# count = 5
+# count.index = 0, 1, 2, 3, 4
 
 resource "aws_security_group_rule" "egress" {
   type              = "egress"
@@ -20,3 +26,4 @@ resource "aws_security_group_rule" "egress" {
   security_group_id = aws_security_group.main_sg.id
   cidr_blocks = ["0.0.0.0/0"]
 }
+
