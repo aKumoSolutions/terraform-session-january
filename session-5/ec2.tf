f2e6185a50a5e7cb34bd6596fee891c678ce2b81
azaa1 <azaa.chi11@gmail.com>
2023-02-11 12:23:37 -0600

    meta argument is added

--- session-5/ec2.tf
+++ session-5/ec2.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "main" {
+    count = 3
     ami = data.aws_ami.amazon_linux_2.id
     instance_type = var.instance_type
     vpc_security_group_ids = [ aws_security_group.main_sg.id ] 
@@ -6,3 +7,5 @@ resource "aws_instance" "main" {
         Name = "${var.env}-instance"           
     }
 }
+
+# count is a meta argument, help you with replicating the resources
