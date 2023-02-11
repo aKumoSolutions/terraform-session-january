b19982d3ed8ad5bef52c9669ecfe16b9d3b922e4
azaa1 <azaa.chi11@gmail.com>
2023-02-11 11:35:18 -0600

    lock table is added

--- session-5/backend.tf
+++ session-5/backend.tf
@@ -3,5 +3,8 @@ terraform {
         bucket = "terraform-session-september-backend-kris"
         region = "us-west-2"
         key    = "session-5/terraform.tfstate" #Where does terraform need to store your file. Path or Prefix 
+        dynamodb_table = "terraform-session-sep-state-lock"  # Lock Table
     }
 }
+
+# 1. You cannot use any variables in key
