9e6850f9771d79017bcae60002783f8778adba47
azaa1 <azaa.chi11@gmail.com>
2023-02-13 18:42:25 -0600

    syntax error is fixed

--- session-6/Filestructure/main.tf
+++ session-6/Filestructure/main.tf
@@ -2,6 +2,6 @@ resource "aws_sqs_queue" "main" {
     name =  "${var.env}-sqs" 
     tags = {
         Environment = var.env
-        Name = format("$s-sqs", var.env)
+        Name = format("%s-sqs", var.env)
     }                    # dev-sqs, qa-sqs, stage-sqs, prod-sqs
 }
