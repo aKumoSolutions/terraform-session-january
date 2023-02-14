9f6681d8e2b9226ae173eea319116f84ecbf2118
azaa1 <azaa.chi11@gmail.com>
2023-02-13 19:07:52 -0600

    backend is updated

--- session-6/Filestructure/backend.tf
+++ session-6/Filestructure/backend.tf
@@ -2,7 +2,11 @@ terraform {
     backend "s3" {
         bucket = "terraform-session-september-backend-kris"
         region = "us-west-2"
-        key = "session-6/filestructure/dev/terraform.tfstate"
+        key = "session-6/filestructure/qa/terraform.tfstate"
         dynamodb_table = "terraform-session-sep-state-lock"
     }
 }
+
+# If you want to isolate infrastructure and resuse your configuration files, you must isolate the backend
+
+# Isolating backend (terraform.tfstate) = Isolating environment
