2bd92fa24d6fde66605c0714e96f33ddf9c91020
azaa1 <azaa.chi11@gmail.com>
2023-02-13 19:22:51 -0600

    switching to dev environment

--- session-6/Filestructure/backend.tf
+++ session-6/Filestructure/backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         bucket = "terraform-session-september-backend-kris"
         region = "us-west-2"
-        key = "session-6/filestructure/qa/terraform.tfstate"
+        key = "session-6/filestructure/dev/terraform.tfstate"
         dynamodb_table = "terraform-session-sep-state-lock"
     }
 }
@@ -10,3 +10,7 @@ terraform {
 # If you want to isolate infrastructure and resuse your configuration files, you must isolate the backend
 
 # Isolating backend (terraform.tfstate) = Isolating environment
+
+# 1 backend at the time
+
+# No variables in key of Backend
