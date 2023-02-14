d15da40a388e155c52dd11c287b195483185b39f
azaa1 <azaa.chi11@gmail.com>
2023-02-13 19:59:09 -0600

    terraform.sh is added

--- session-6/Filestructure/backend.tf
+++ session-6/Filestructure/backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         bucket = "terraform-session-september-backend-kris"
         region = "us-west-2"
-        key = "session-6/filestructure/dev/terraform.tfstate"
+        key = "session-6/filestructure/_env_/terraform.tfstate"
         dynamodb_table = "terraform-session-sep-state-lock"
     }
 }
