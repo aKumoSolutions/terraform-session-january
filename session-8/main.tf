0b1446daa6f1cf9755f3517e2f1c77fcd6a011ca
azaa1 <azaa.chi11@gmail.com>
2023-02-20 20:02:54 -0600

    local exec is added

--- session-8/main.tf
+++ session-8/main.tf
@@ -38,3 +38,9 @@ resource "aws_key_pair" "terraform_server" {
 output "instance_ip" {
     value = aws_instance.main.public_ip
 }
+
+resource "null_resource" "local_script" {
+    provisioner "local-exec" {
+      command = "echo 'Hello from the Local Server' >> local.txt"
+    }
+}
