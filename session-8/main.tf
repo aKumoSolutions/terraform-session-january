65ed1b7583ee48c852e4478b2eea20dadb638bfc
azaa1 <azaa.chi11@gmail.com>
2023-02-20 19:08:39 -0600

    provisioner block is addded

--- session-8/main.tf
+++ session-8/main.tf
@@ -3,7 +3,21 @@ resource "aws_instance" "main" {
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.main_sg.id]
     key_name = aws_key_pair.terraform_server.key_name
+
+    provisioner "file" {
+        source = "/home/ec2-user/terraform-session-january/session-8/index.html" # path on Terraform Server
+        destination = "/tmp/index.html" # path for the Remote Server
+
+        connection {
+            type = "ssh"
+            user = "ec2-user" # Remote Server User
+            host = self.public_ip
+            private_key = file("~/.ssh/id_rsa")
+        }
+    }
 }
+ 
+# ssh user@ip, it automatically checks public key on the remote machine, private key on my local machine
 
 resource "aws_key_pair" "terraform_server" {
     key_name = "Terraform-Server-Key"
