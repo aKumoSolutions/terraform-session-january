18d8a06d706f514078fe50a6ca006e9e143cd927
azaa1 <azaa.chi11@gmail.com>
2023-02-25 12:11:12 -0600

    removed main

--- session-10 /main.tf
+++ session-10 /main.tf
@@ -1,7 +1,7 @@
 # Calling a child module remotely 
 
 module "ec2" {
-    source = "github.com/aKumoSolutions/terraform-session-january/main/modules/ec2"
+    source = "github.com/aKumoSolutions/terraform-session-january/modules/ec2"
     env = "dev"
     ami = "ami-06e85d4c3149db26a" 
     instance_type = "t2.micro"
@@ -10,6 +10,6 @@ module "ec2" {
 # /aKumoSolutions = Organization
 # /terraform-session-january = Repository
 # /tree = 
-# /main = Branch
+# /main = Branch (default)
 # /modules = Subdirectory
 # /ec2 = Subdirectory
