031fabc3abdea68fdf3ae211b3b18759bac4e7a6
azaa1 <azaa.chi11@gmail.com>
2023-02-25 11:41:08 -0600

    commented

--- session-10 /main.tf
+++ session-10 /main.tf
@@ -6,10 +6,10 @@ module "ec2" {
     ami = "ami-06e85d4c3149db26a" 
     instance_type = "t2.micro"
 }
-github.com = Domain Name (SCM)
-/aKumoSolutions = Organization
-/terraform-session-january = Repository
-/tree = 
-/main = Branch
-/modules = Subdirectory
-/ec2 = Subdirectory
+# github.com = Domain Name (SCM)
+# /aKumoSolutions = Organization
+# /terraform-session-january = Repository
+# /tree = 
+# /main = Branch
+# /modules = Subdirectory
+# /ec2 = Subdirectory
