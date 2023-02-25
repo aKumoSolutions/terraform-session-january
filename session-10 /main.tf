64ea735886f0abb9b3e9b407406973377a2f747f
azaa1 <azaa.chi11@gmail.com>
2023-02-25 12:41:57 -0600

    / is added for source

--- session-10 /main.tf
+++ session-10 /main.tf
@@ -1,7 +1,7 @@
 # Calling a child module remotely 
 
 module "ec2" {
-    source = "github.com/aKumoSolutions/terraform-session-january/modules/ec2?ref=v1.0.0"
+    source = "github.com/aKumoSolutions/terraform-session-january//modules/ec2?ref=v1.0.0"
     env = "dev"
     ami = "ami-06e85d4c3149db26a" 
     instance_type = "t2.micro"
