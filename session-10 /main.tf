5ec9393b89c1eaffa6916f3ee2fd2fe880c47e40
azaa1 <azaa.chi11@gmail.com>
2023-02-25 12:54:43 -0600

    updated

--- session-10 /main.tf
+++ session-10 /main.tf
@@ -1,10 +1,16 @@
 # Calling a child module remotely 
-
+# Github
 module "ec2" {
-    source = "github.com/aKumoSolutions/terraform-session-january//modules/ec2?ref=v1.0.0"
+    source = "github.com/aKumoSolutions/terraform-session-january//modules/ec2?ref=v1.0.1"
     env = "dev"
     ami = "ami-06e85d4c3149db26a" 
     instance_type = "t2.micro"
+}
+
+# Terraform Registry 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.18.0"
 }
 # github.com = Domain Name (SCM)
 # /aKumoSolutions = Organization
