5c096fd032eec67ca3f1e452c02be0436017a74d
azaa1 <azaa.chi11@gmail.com>
2023-02-27 20:52:25 -0600

    The End

--- session-11/main.tf
+++ session-11/main.tf
@@ -3,3 +3,10 @@ resource "aws_sqs_queue" "main" {
 }
 
 # terraform.workspace = workspace name
+
+# For Each, For loop in Terraform
+# Dynamic Resource Creation in Terraform
+# Terraform CLI vs Cloud vs Enterprise
+# Terraform validate, fmt, import, providers, refresh, state, taint, show
+# Environment Variables - TF_LOG, TF_LOG_PATH, TF_INPUT, TF_VAR_name, TF_CLI_ARGS,
+# Functions - slice, concat, lookup, file
